e95a9b07b68c90278681faac7eb0483ed4fbd7d7
Kyle Benesch <4b796c65+github@gmail.com>
2023-03-10 16:51:10 -0800

    Update Mypy.  Remove outdated ignores.

--- tcod/ec/__init__.py
+++ tcod/ec/__init__.py
@@ -188,7 +188,7 @@ class ComponentDict:
             real_key is key
         ), f"{key!r} is a child of an abstract component and can only be accessed with {real_key!r}."
 
-    def set(self, *components: object) -> Self:  # type: ignore[valid-type]  # Needs mypy >0.991
+    def set(self, *components: object) -> Self:
         """Assign or replace the components of this entity and return self.
 
         .. versionchanged:: 1.1
