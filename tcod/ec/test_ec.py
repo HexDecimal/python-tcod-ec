de9359a99187492d93951452d22b4ebd576f1892
Kyle Benesch <4b796c65+github@gmail.com>
2023-06-29 15:24:46 -0700

    Add pre-commit hook.
    Fix new Ruff warnings.

--- tcod/ec/test_ec.py
+++ tcod/ec/test_ec.py
@@ -85,7 +85,7 @@ def test_ComponentDict() -> None:
     assert set(entity) == entity.keys() == {Base, Foo}
     assert set(entity.values()) == {derived, foo}
     assert set(zip(entity.keys(), entity.values())) == set(entity.items())
-    assert entity == entity
+    assert entity == entity  # noqa: PLR0124
     assert entity != tcod.ec.ComponentDict(entity.values())
 
     entity = tcod.ec.ComponentDict()
